84797f66245db828452728c90d69b253a33c3416
mehemmedmemmedli2005 <mehemmed.memmedli.05@gmail.com>
2024-12-26 02:23:13 +0400

    Delete Polling_System/app directory

--- Polling_System/app/models/option.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-class Option(BaseModel):
-    option_id: int
-    poll_id: int
-    name: str
-    votes: int = 0
--- Polling_System/app/models/poll.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-from typing import List
-
-class Poll(BaseModel):
-    poll_id: int
-    question: str
-    options: List[str]  # List of option names
-    is_active: bool = True
